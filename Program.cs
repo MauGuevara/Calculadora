b43619b2ef5a219208287ce426a8894ed1124feb
luisMontanoRojas <lushio142356@gmail.com>
2020-09-23 20:53:52 -0400

    cleanned code.

--- Program.cs
+++ Program.cs
@@ -1,41 +1,94 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Calculadora
 {
     class Program
     {
+        public static double operation, firstNumber, secondNumber, result;
+
         static void Main(string[] args)
         {
-            double q, w, r;
-            r = 0;
-            string p;
+            menu();
+        }
+
+		static void menu()
+		{
+            do
+			{
+                Console.Clear();
+                Console.WriteLine("----------CALCULADORA----------");
+                Console.WriteLine("1.- Sumar");
+                Console.WriteLine("2.- Restar");
+                Console.WriteLine("3.- Multiplicar");
+                Console.WriteLine("4.- Dividir");
+                Console.WriteLine("0.- Salir");
+                Console.WriteLine("-------------------------------");
+                operation = int.Parse(Console.ReadLine());
+
+				switch (operation)
+				{
+					case 1:
+                        enterNumber();
+                        add();
+                        showResult();
+                        break;
+
+					case 2:
+                        enterNumber();
+                        subtract();
+                        showResult();
+                        break;
+
+                    case 3:
+                        enterNumber();
+                        multiply();
+                        showResult();
+                        break;
+
+                    case 4:
+                        enterNumber();
+                        divide();
+                        showResult();
+                        break;
+				}
+
+			} while (operation != 0);
+
+		}
+
+        static void enterNumber()
+        {
             Console.WriteLine("ingresa primer numero");
-            q= int.Parse(Console.ReadLine());
+            firstNumber = int.Parse(Console.ReadLine());
             Console.WriteLine("ingresa segundo numero");
-            w = int.Parse(Console.ReadLine());
-            Console.WriteLine("ingresa operador");
-            p = Console.ReadLine();
+            secondNumber = int.Parse(Console.ReadLine());
+        }
 
-            switch (p)
-            {
-                case "+":
-                    r = q + w;
-                    break;
-                case "-":
-                    r = q - w;
-                    break;
-                case "*":
-                    r = q * w;
-                    break;
-                case "/":
-                    r = q / w;
-                    break;
-                default:
-                    Console.WriteLine("ingresar datos validos");
-                    break;
-            }
+        static void add() 
+        {
+            result = firstNumber + secondNumber;
+        }
 
-            Console.WriteLine($"el resultado es {r}");
+        static void subtract()
+        {
+            result = firstNumber - secondNumber;
+        }
+
+        static void multiply()
+        {
+            result = firstNumber * secondNumber;
+        }
+
+        static void divide()
+        {
+            result = firstNumber / secondNumber;
+        }
+
+        static void showResult()
+        {
+            Console.WriteLine($"El resultado es: {result}");
+            Console.ReadLine();
         }
     }
 }
